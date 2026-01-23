0a5e21d45f319e856a3e5b1031ee8b82b863f1e8
orangegegege <charles96223@gmail.com>
2026-01-24 03:36:06 +0800

    app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ PAGE_BG_COLOR = "#E89B00"
 
 # 3. 導覽列 (Header) 配色
 # 建議：深色 (#2D3436) 或 品牌色 (#E89B00)
-NAV_BG_COLOR = "#E89B00"
+NAV_BG_COLOR = "#2D3436"
 NAV_TEXT_COLOR = "#FFFFFF"
 
 # 4. 內容卡片 (Card) 配色
@@ -293,5 +293,6 @@ def login_page():
 
 if st.session_state.current_page == "login": login_page()
 else: main_page()
+
 
 
